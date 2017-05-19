c5aecf1dc951b6bbd63aaad9d92c6bd59dceebe7
Yoctopuce <dev@yoctopuce.com>
2017-05-19 21:47:13 +0200

    "New build based on revison 27564"

--- example_nodejs/Prog-WebSocketCallback/demo.js
+++ example_nodejs/Prog-WebSocketCallback/demo.js
@@ -2,38 +2,71 @@
 
 require('yoctolib-es2017/yocto_api.js');
 
-let WebSocketServer = require('ws').Server;
+YAPI.LogUnhandledPromiseRejections();
+
+var Data = [];
+
+async function sensorCallback(sensor, measure)
+{
+  Data.push({
+    "name" : await sensor.get_hardwareId(),
+    "time" : await measure.get_endTimeUTC(),
+    "value" : await measure.get_averageValue()
+  });
+}
+
+async function HttpCallbackHandler(request, response)
+{
+  // HTTP requests from the web browsers land here
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('time;sensor;value<br>\n');
+  for(let i = 0; i < Data.length; i++) {
+    response.write(Data[i].time + ";" +
+      Data[i].name + ";" +
+      Data[i].value + "<br>\n");
+  }
+  response.end();
+}
 
 async function WebSocketCallbackHandler(ws)
 {
-    // Here you can filter the requests by URL if you want
-    console.log('Received websocket request');
-
-    // The part below starts the Yoctopuce library in WebSocket Callback mode and interacts
-    // with modules connected on the VirtualHub or YoctoHub that made the HTTP request
-    let errmsg = new YErrorMsg();
-    let yctx = new YAPIContext();
-    if(await yctx.RegisterHubWebSocketCallback(ws, errmsg) != YAPI.SUCCESS) {
-        console.log('HTTP callback error: '+errmsg);
-        yctx.FreeAPI();
-        return;
-    }
-
-    // Display a list of modules on incoming hub to the Node.js console
+  // WebSocket connections from the YoctoHubs land here
+  console.log('Incoming WebSocket connection!');
+  let errmsg = new YErrorMsg();
+  let yctx = new YAPIContext();
+  if(await yctx.RegisterHubWebSocketCallback(ws, errmsg) != YAPI.SUCCESS) {
+    console.log('WebSocket callback error: ' + errmsg);
+    yctx.FreeAPI();
+    return;
+  }
+  try {
     await yctx.UpdateDeviceList(errmsg);
-    let module = YModule.FirstModuleInContext(yctx);
-    while(module) {
-        let msg = (await module.get_serialNumber()) + ' (' + (await module.get_productName()) + ')';
-        console.log(msg);
-        module = module.nextModule();
+    let sensor = YSensor.FirstSensorInContext(yctx);
+    while(sensor != null) {
+      console.log('Sensor: ' + (await sensor.get_hardwareId()));
+      await sensor.set_reportFrequency("6/m");
+      await sensor.registerTimedReportCallback(sensorCallback);
+      sensor = sensor.nextSensor()
     }
-    await yctx.FreeAPI();
+    while(ws.readyState != ws.CLOSED) {
+      await yctx.Sleep(1000, errmsg);
+    }
+  } catch(e) {
+    console.log('Exception in WebSocket handler:');
+    console.log(e);
+  }
+  console.log('Websocket disconnected');
+  await yctx.FreeAPI();
 }
 
-YAPI.LogUnhandledPromiseRejections();
+// We create an HTTP server...
+let http = require('http');
+http.createServer(HttpCallbackHandler).listen(8080);
 
-// Instantiate a simple HTTP server
+// ... and we create a WebSocket server
+let WebSocketServer = require('ws').Server;
 let wss = new WebSocketServer({ port: 8044 });
 wss.on('connection', WebSocketCallbackHandler);
 
-console.log('Node.js WebSocket Callback server running at http://...:8044/');
+console.log('Node.js Web HTTP server running on http://...:8080/');
+console.log('Node.js Websocket server running on http://...:8044/');
