13a1d26d8877b5c629c9f320258580bfaf12eeaf
Yoctopuce <dev@yoctopuce.com>
2019-06-04 13:18:50 +0200

    "New build based on revison 35622"

---
+++ example_nodejs/Doc-GettingStarted-Yocto-Pressure/demo.js
@@ -0,0 +1,63 @@
+/*********************************************************************
+ *
+ *  $Id: svn_id $
+ *
+ *  An example that show how to use a  Yocto-Pressure
+ *
+ *  You can find more information on our web site:
+ *   Yocto-Pressure documentation:
+ *      https://www.yoctopuce.com/EN/products/yocto-pressure/doc.html
+ *   EcmaScript API Reference:
+ *      https://www.yoctopuce.com/EN/doc/reference/yoctolib-ecmascript-EN.html
+ *
+ *********************************************************************/
+
+"use strict";
+
+require('yoctolib-es2017/yocto_api.js');
+require('yoctolib-es2017/yocto_pressure.js');
+
+let press;
+
+async function startDemo()
+{
+    await YAPI.LogUnhandledPromiseRejections();
+    await YAPI.DisableExceptions();
+
+    // Setup the API to use the VirtualHub on local machine
+    let errmsg = new YErrorMsg();
+    if(await YAPI.RegisterHub('127.0.0.1', errmsg) != YAPI.SUCCESS) {
+        console.log('Cannot contact VirtualHub on 127.0.0.1: '+errmsg.msg);
+        return;
+    }
+
+    // Select specified device, or use first available one
+    let serial = process.argv[process.argv.length-1];
+    if(serial[8] != '-') {
+        // by default use any connected module suitable for the demo
+        let anysensor = YPressure.FirstPressure();
+        if(anysensor) {
+            let module = await anysensor.module();
+            serial = await module.get_serialNumber();
+        } else {
+            console.log('No matching sensor connected, check cable !');
+            return;
+        }
+    }
+    console.log('Using device '+serial);
+    press = YPressure.FindPressure(serial+".pressure");
+
+    refresh();
+}
+
+async function refresh()
+{
+    if (await press.isOnline()) {
+        console.log('Pressure : '+(await press.get_currentValue()) + (await press.get_unit()));
+    } else {
+        console.log('Module not connected');
+    }
+    setTimeout(refresh, 500);
+}
+
+startDemo();
